a26de1d188f45c898c4142abcdcb7b71169f8f50
Nightscape Coffee <0c48d607@opayq.com>
2021-03-22 06:36:57 -0700

    wroles: Finish remove, Start roles

--- wroles/wroles.py
+++ wroles/wroles.py
@@ -11,12 +11,12 @@ class wroles(commands.Cog):
     def __init__(self):
         self.config = Config.get_conf(self, identifier=822775204043948063)
         default_guild = {
-            "approvedRoles": [],
+            "roleList": [],
             "roleMsgs": []
         }
 
         """ template
-        "approvedroles": [ role_id, role_id, ... ],
+        "rolelist": [ role_id, role_id, ... ],
         "rolemsgs": [
             {
                 "name": "name",
@@ -43,38 +43,49 @@ class wroles(commands.Cog):
 
     # Utility Commands
 
-    async def addToApprovedRoles(self, ctx, roleName: discord.Role):
-        """Add a role to the list of approved roles"""
-        currentRoles = await self.config.guild(ctx.guild).approvedRoles()
+    async def addToRoleList(self, ctx, roleName: discord.Role):
+        """Add a role to the list of roles"""
+        currentRoles = await self.config.guild(ctx.guild).roleList()
         currentRoles.append(roleName)
-        await self.config.guild(ctx.guild).approvedRoles.set(currentRoles)
+        await self.config.guild(ctx.guild).roleList.set(currentRoles)
+        return True
+
+    async def removeFromRoleList(self, ctx, roleName: discord.Role):
+        """Remove a role from the list of roles"""
+        currentRoles = await self.config.guild(ctx.guild).roleList()
+        currentRoles.remove(roleName)
+        await self.config.guild(ctx.guild).roleList.set(currentRoles)
         return True
 
 
     # Bot Commands
 
-    @commands.command(name="roles")
-    async def wroles(self, ctx, role: discord.Role=None):
+    @commands.command(name="roles", aliases=["iam"])
+    async def wroles(self, ctx, *, role: discord.Role=None):
         """List self-assignable Event Roles
         
         Add roles using `[p]roles roleYouWantToAddHere`"""
         if role == None:
-            # [TODO] Output list of roles if role is empty
-            await ctx.send("No role specified")
+            currentRoles = await self.config.guild(ctx.guild).roleList()
+            # [TODO] Create an embed of these roles, with @mentions
+            await ctx.send(currentRoles)
         else:
-            # [TODO] Add role to user
-            await ctx.send(role)
+            # [TODO] Add check for if role is in roleList
+            await ctx.author.add_roles(role)
+            await ctx.message.add_reaction("✅")
     
     @commands.guild_only()
     @commands.group()
     @checks.mod()
-    async def treeroles(self, ctx: commands.Context):
-        """Role management for Trees"""
+    async def setroles(self, ctx: commands.Context):
+        """Role management for Trees and Contributor Hub
+        
+        To list all roles, do `[p]roles`"""
         if not ctx.invoked_subcommand:
             pass
 
-    @treeroles.command(name="create")
-    async def treerolescreate(self, ctx, *, roleName):
+    @setroles.command(name="create")
+    async def setrolescreate(self, ctx, *, roleName):
         """Create a pingable Event Role (for Cafe Events and #hangouts)
         
         Please make sure your event shows demonstrated interest first!
@@ -85,40 +96,40 @@ class wroles(commands.Cog):
         # role = await ctx.guild.create_role(name=roleName, mentionable=True, colour = discord.Colour(int(f"0x{hexColor}", 16)))
 
         role = await ctx.guild.create_role(name=roleName, mentionable=True)
-        await self.addToApprovedRoles(ctx, role)
+        await self.addToRoleList(ctx, role)
         await ctx.send(f"{role.mention} is created and ready for use! Add it using `,wroles`")
         
-    @treeroles.command(name="add")
+    @setroles.command(name="add")
     @checks.admin()
-    async def treerolesadd(self, ctx, *, roleName: discord.Role):
+    async def setrolesadd(self, ctx, *, roleName: discord.Role):
         """Adds an existing role to the list of Event Roles."""
-        await self.addToApprovedRoles(ctx, roleName)
+        await self.addToRoleList(ctx, roleName)
         await ctx.message.add_reaction("✅")
         
-    # @treeroles.command(name="remove")
-    # @checks.admin()
-    # async def treerolesremove(self, ctx, *, roleName: discord.Role):
-    #     """Removes an existing role to the list of Event Roles."""
-    #     await self.addToApprovedRoles(ctx, roleName)
-    #     await ctx.message.add_reaction("✅")
-        
-    @treeroles.command(name="enable")
+    @setroles.command(name="remove")
     @checks.admin()
-    async def treerolesenable(self, ctx, *, roleName: discord.Role):
+    async def setrolesremove(self, ctx, *, roleName: discord.Role):
+        """Removes an existing role from the list of Event Roles."""
+        await self.removeFromRoleList(ctx, roleName)
+        await ctx.message.add_reaction("✅")
+        
+    @setroles.command(name="enable")
+    @checks.admin()
+    async def setrolesenable(self, ctx, *, roleName: discord.Role):
         """Make an Event Role mentionable.
         
         Only for if you've made it unmentionable using disable"""
         await roleName.edit(mentionable=True)
         await ctx.message.add_reaction("✅")
         
-    @treeroles.command(name="disable")
+    @setroles.command(name="disable")
     @checks.admin()
-    async def treerolesdisable(self, ctx, *, roleName: discord.Role):
+    async def setrolesdisable(self, ctx, *, roleName: discord.Role):
         """Make an Event Role unmentionable. Does not delete the role."""
         await roleName.edit(mentionable=False)
         await ctx.message.add_reaction("✅")
 
-    @treeroles.command(name="edit")
-    async def treerolesedit(self, ctx, *, roleName: discord.Role):
+    @setroles.command(name="edit")
+    async def setrolesedit(self, ctx, *, roleName: discord.Role):
         """Edit an Event Role"""
         # [TODO] Add support for await self.wait_for(hex, check=x, timeout=10.0)
